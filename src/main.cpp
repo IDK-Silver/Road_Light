5b3de42bfc7c3477c8c38d91bee24526f8c8700a
黃祥毅 <60951976+IDK-Silver@users.noreply.github.com>
2022-03-16 09:15:04 +0800

    Format Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
-
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "./image_data.h"
+#include <ImageDatas.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
